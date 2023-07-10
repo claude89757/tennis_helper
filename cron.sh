45cad38ea1614404d12e498a3f8051fdf5aa42f0
claude89757 <claudexie@outlook.com>
2023-07-10 23:53:04 +0800

    fix

--- cron.sh
+++ cron.sh
@@ -8,10 +8,10 @@ TENCENT_CLOUD_SECRET_ID=""
 TENCENT_CLOUD_SECRET_KEY=""
 
 # run tennis tools
-*/5 * * * * /usr/bin/python3 /home/lighthouse/watcher_for_isz.py --item_name "大沙河" --sales_id "100220" --sales_item_id "100000" --watch_days 7 --send_sms 1 >> /home/lighthouse/logs/xm_$(date +\%Y-\%m-\%d-\%H\%M).log 2>&1
-*/5 * * * * /usr/bin/python3 /home/lighthouse/watcher_for_isz.py --item_name "香蜜公园" --sales_id "101332" --sales_item_id "100341" --watch_days 2 --send_sms 1 >> /home/lighthouse/logs/xm_$(date +\%Y-\%m-\%d-\%H\%M).log 2>&1
-*/5 * * * * /usr/bin/python3 /home/lighthouse/watcher_for_isz.py --item_name "深云文体" --sales_id "105057" --sales_item_id "105127" --watch_days 2 --send_sms 1 >> /home/lighthouse/logs/xm_$(date +\%Y-\%m-\%d-\%H\%M).log 2>&1
-*/5 * * * * /usr/bin/python3 /home/lighthouse/watcher_for_isz.py --item_name "黄木岗" --sales_id "101333" --sales_item_id "100344" --watch_days 2 --send_sms 1 >> /home/lighthouse/logs/xm_$(date +\%Y-\%m-\%d-\%H\%M).log 2>&1
+*/5 * * * * /usr/bin/python3 /home/lighthouse/tennis_helper/watcher_for_isz.py --item_name "大沙河" --sales_id "100220" --sales_item_id "100000" --watch_days 7 --send_sms 1 >> /home/lighthouse/logs/xm_$(date +\%Y-\%m-\%d-\%H\%M).log 2>&1
+*/5 * * * * /usr/bin/python3 /home/lighthouse/tennis_helper/watcher_for_isz.py --item_name "香蜜公园" --sales_id "101332" --sales_item_id "100341" --watch_days 2 --send_sms 1 >> /home/lighthouse/logs/xm_$(date +\%Y-\%m-\%d-\%H\%M).log 2>&1
+*/5 * * * * /usr/bin/python3 /home/lighthouse/tennis_helper/watcher_for_isz.py --item_name "深云文体" --sales_id "105057" --sales_item_id "105127" --watch_days 2 --send_sms 1 >> /home/lighthouse/logs/xm_$(date +\%Y-\%m-\%d-\%H\%M).log 2>&1
+*/5 * * * * /usr/bin/python3 /home/lighthouse/tennis_helper/watcher_for_isz.py --item_name "黄木岗" --sales_id "101333" --sales_item_id "100344" --watch_days 2 --send_sms 1 >> /home/lighthouse/logs/xm_$(date +\%Y-\%m-\%d-\%H\%M).log 2>&1
 
 
 # delete old logs everyday
