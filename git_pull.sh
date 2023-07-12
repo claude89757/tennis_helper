328cd57c67bd0dd635d83c277afa7084e1e3842d
claude89757 <claudexie@outlook.com>
2023-07-13 01:24:11 +0800

    fix

--- git_pull.sh
+++ git_pull.sh
@@ -3,8 +3,11 @@
 # 定义目录路径
 dir_path="/home/lighthouse/tennis_helper"
 
+# 定义日志文件路径
+log_file="$1"
+
 # 进入目录
 cd $dir_path
 
-# 执行git pull
-git pull
+# 执行git pull并将输出重定向到日志文件，每次执行都覆盖之前的日志
+git pull > $log_file 2>&1
