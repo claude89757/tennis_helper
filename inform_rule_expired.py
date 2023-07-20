feeb75a8f3a1970c5b8cf7ee5b0938d76ff97a23
claude89757 <claudexie@outlook.com>
2023-07-20 20:58:53 +0800

    fix: 修复重复通知过期的问题

--- inform_rule_expired.py
+++ inform_rule_expired.py
@@ -9,6 +9,7 @@
 
 from weda import get_all_rule_list
 from sms import send_sms_for_news
+from weda import update_record_info_by_id
 
 if __name__ == '__main__':
     # 获取订阅列表
@@ -16,21 +17,38 @@ if __name__ == '__main__':
 
     # 整理每个用户的订阅状态
     phone_rule_status_infos = {}
+    phone_rule_infos = {}
     for rule in rule_list:
         phone = rule['phone']
         status = rule['status']
         if phone_rule_status_infos.get(phone):
             phone_rule_status_infos[phone].append(status)
+            phone_rule_infos[phone].append(rule)
         else:
             phone_rule_status_infos[phone] = [status]
+            phone_rule_status_infos[phone] = [rule]
 
     # 判断哪些用户的全部订阅都过期，如果是，发送短信提醒
     inform_phone_list = []
     for phone, rule_status_set in phone_rule_status_infos.items():
         if len(set(rule_status_set)) == 1 and rule_status_set[0] == '3':
-            sms_res = send_sms_for_news([phone], [f"{len(rule_status_set)} 个"], template_id="1863727")
-            print(sms_res)
-            inform_phone_list.append(phone)
+            is_send_before = set()
+            for rule in phone_rule_infos[phone]:
+                is_send_before.add(str(rule.get('tzgq')))
+            if len(is_send_before) == 1 and list(is_send_before)[0] == '2':
+                # 已通知过，跳过
+                pass
+            else:
+                # 未通知过，需要通知
+                sms_res = send_sms_for_news([phone], [f"{len(rule_status_set)} 个"], template_id="1863727")
+                print(sms_res)
+                inform_phone_list.append(phone)
+                # 标记已通知过
+                for rule in phone_rule_infos[phone]:
+                    if rule.get("tzgq") and rule.get("tzgq") == '2':
+                        pass
+                    else:
+                        update_record_info_by_id(rule['_id'], {"tzgq": '2'})
         else:
             pass
     print(f"send sms to {inform_phone_list} phone")
