ec742d20cd8b0040c0313066b977b93d73ced8d0
claude89757 <claudexie@outlook.com>
2023-07-28 13:59:51 +0800

    fix

--- update_docs_for_rule.py
+++ update_docs_for_rule.py
@@ -51,7 +51,7 @@ if __name__ == '__main__':
     # 获取所有规则
     all_rule_list = get_all_rule_list()
 
-    sorted_rule_list = sorted(all_rule_list, key=lambda x: x['createdAt'], reverse=False)
+    sorted_rule_list = sorted(all_rule_list, key=lambda x: x['createdAt'], reverse=True)
 
     index = 2
     for rule in sorted_rule_list:
