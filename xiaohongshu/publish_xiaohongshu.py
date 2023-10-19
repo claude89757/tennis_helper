3f357106bc54dd08104e314733c49d4b00f9031a
claude89757 <claudexie@outlook.com>
2023-10-19 12:49:05 +0800

    fix

--- xiaohongshu/publish_xiaohongshu.py
+++ xiaohongshu/publish_xiaohongshu.py
@@ -167,7 +167,7 @@ def tencent_docs_to_image(image_path: str):
 
     # 等待JavaScript加载
     print("waiting for 10s ...")
-    time.sleep(10)
+    time.sleep(15)
 
     # 获取要截屏的元素
     print(f"finding element...")
