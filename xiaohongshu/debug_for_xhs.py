e7a7836a49316e152900ae639130711ad614bb8b
claude89757 <claudexie@outlook.com>
2024-07-19 01:58:23 +0800

    fix

--- xiaohongshu/debug_for_xhs.py
+++ xiaohongshu/debug_for_xhs.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 
 # 如果Geckodriver不在系统路径中，需要指定路径
-# service = Service(executable_path='/usr/local/bin/geckodriver')
+service = Service(executable_path='/usr/local/bin/geckodriver')
 
 # 创建Firefox WebDriver实例
 driver = webdriver.Firefox()  # 如果需要指定service，加入参数：service=service
