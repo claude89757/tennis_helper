2cf54fd93d44326443a7fb5f6ed5ff88e26120e4
claude89757 <claudexie@outlook.com>
2024-07-22 10:12:09 +0800

    fix

--- scf/get_sz_tennis_court_data.py
+++ scf/get_sz_tennis_court_data.py
@@ -286,9 +286,9 @@ def main_handler(event, context):
                     else:
                         pass
             if is_input_time_range_free:
-                output_data = f"【{place_name}】 {formatted_date_str} {start_time}-{end_time} 空闲中"
+                output_data = f"【{place_name}】 {formatted_date_str} {start_time}-{end_time} 空闲中🟢"
             else:
-                output_data = f"【{place_name}】 {formatted_date_str} {start_time}-{end_time} 已被预定"
+                output_data = f"【{place_name}】 {formatted_date_str} {start_time}-{end_time} 已被预定😭"
             return {"code": 0, "data": output_data, "msg": "success"}
         except Exception as error:  # pylint: disable=broad-except
             return {"code": -1, "data": None, "msg": str(error)}
