9f5260b4b9d0cb7f122c56e93b9490b202120c60
claude89757 <claudexie@outlook.com>
2024-08-15 02:31:35 +0800

    fix

--- api/app.py
+++ api/app.py
@@ -1,41 +1,31 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-@Time    : 2024/7/28 17:45
-@Author  : claudexie
-@File    : app.py
-@Software: PyCharm
-"""
 from flask import Flask, jsonify
 import os
 
 app = Flask(__name__)
 
 
-@app.route('/available_courts', methods=['GET'])
-def get_available_courts():
-    """
-    查询 /root 文件夹下的 available_court.txt 结尾的文件，并返回其内容
-    """
-    local_path = "/root"
-    matching_files = []
+@app.route('/api/files', methods=['GET'])
+def get_files():
+    directory = '/root'
+    files_info = []
 
-    try:
-        for root, dirs, files in os.walk(local_path):
-            for file in files:
-                if file.endswith('available_court.txt'):
-                    file_path = os.path.join(root, file)
-                    with open(file_path, 'r') as f:
-                        file_content = f.read()
-                    matching_files.append({
-                        'filename': file_path,
-                        'content': file_content
-                    })
+    # 遍历目录中的文件
+    for filename in os.listdir(directory):
+        if filename.endswith('available_court.txt'):
+            file_path = os.path.join(directory, filename)
+            try:
+                with open(file_path, 'r') as file:
+                    file_content = file.read()
+                files_info.append({
+                    'filename': file_path,
+                    'content': file_content
+                })
+            except Exception as e:
+                return jsonify({'error': str(e)}), 500
 
-        return jsonify({"data": matching_files})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    return jsonify(files_info)
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
