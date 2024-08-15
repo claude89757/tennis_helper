5abc1fc4606778e82a0f456ee39c41e750119074
claude89757 <claudexie@outlook.com>
2024-08-15 22:30:49 +0800

    fix

--- api/app.py
+++ api/app.py
@@ -1,21 +1,19 @@
-from flask import Flask, jsonify
+from quart import Quart, jsonify
 import os
 
-app = Flask(__name__)
-
+app = Quart(__name__)
 
 @app.route('/api/files', methods=['GET'])
-def get_files():
+async def get_files():
     directory = '/root'
     files_info = []
 
-    # 遍历目录中的文件
     for filename in os.listdir(directory):
         if filename.endswith('available_court.txt'):
             file_path = os.path.join(directory, filename)
             try:
-                with open(file_path, 'r') as file:
-                    file_content = file.read()
+                async with aiofiles.open(file_path, 'r') as file:
+                    file_content = await file.read()
                 files_info.append({
                     'filename': file_path,
                     'content': file_content
@@ -25,7 +23,5 @@ def get_files():
 
     return jsonify(files_info)
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
