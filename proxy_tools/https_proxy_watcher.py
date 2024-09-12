1a08bbb2f9187cf22ad96fb8b75be846c96c9933
claude89757 <claudexie@outlook.com>
2024-09-13 01:22:31 +0800

    add proxy tools

---
+++ proxy_tools/https_proxy_watcher.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2024-7-20 02:35:46
+@Author  : claude89757
+@File    : https_proxy_watcher.py
+@Software: PyCharm
+"""
+import os
+import requests
+import datetime
+import random
+import base64
+
+
+FILENAME = "isz_https_proxies.txt"
+
+
+def generate_proxies():
+    """
+    获取待检查的代理列表
+    """
+    urls = [
+        "https://github.com/roosterkid/openproxylist/raw/main/HTTPS_RAW.txt",
+        "https://raw.githubusercontent.com/yoannchb-pro/https-proxies/main/proxies.txt",
+        "https://raw.githubusercontent.com/Zaeem20/FREE_PROXIES_LIST/master/https.txt",
+        "https://raw.githubusercontent.com/ErcinDedeoglu/proxies/main/proxies/https.txt",
+    ]
+    proxies = []
+
+    proxy_url_infos = {}
+    for url in urls:
+        print(f"getting proxy list for {url}")
+        response = requests.get(url)
+        # 将文本内容按行分割，并去除每行两端的空格
+        text = response.text.strip()
+        lines = text.split("\n")
+        lines = [line.strip() for line in lines]
+        proxies.extend(lines)
+        print(f"Loaded {len(lines)} proxies from {url}")
+        for line in lines:
+            proxy_url_infos[line] = url
+    print(f"Total {len(proxies)} proxies loaded")
+    random.shuffle(proxies)
+    return proxies, proxy_url_infos
+
+
+def check_proxy(proxy_url, proxy_url_infos):
+    """
+    检查代理是否可用
+    """
+    try:
+        print(f"Checking {proxy_url}")
+        target_url = os.environ['ISZ_URL']
+        response = requests.get(target_url, proxies={"https": f"http://{proxy_url}"}, timeout=3)
+        print(str(response.text)[:100])
+        if response.status_code == 200 and "html" in str(response.text):
+            print(f"[OK]  {proxy_url}, from {proxy_url_infos.get(proxy_url)}")
+            return proxy_url
+        if response.status_code == 200 and response.json()['code'] == -1 \
+                and response.json()['msg'] == '签名错误,接口未签名':
+            print(f"[OK] {proxy_url} from {proxy_url_infos.get(proxy_url)}")
+            return proxy_url
+    except Exception as error:
+        print(str(error).split()[0])
+    return None
+
+
+def update_proxy_file(filename, available_proxies):
+    # 读取现有的代理列表
+    try:
+        with open(filename, "r") as file:
+            existing_proxies = file.readlines()
+    except FileNotFoundError:
+        existing_proxies = ""
+
+    # 清理现有代理列表中的换行符
+    existing_proxies = [proxy.strip() for proxy in existing_proxies]
+
+    # 过滤出不在文件中的新代理
+    new_proxies = [proxy for proxy in available_proxies if proxy not in existing_proxies]
+
+    # 如果有新的有效代理，追加到文件
+    if new_proxies:
+        with open(filename, "a") as file:
+            for proxy in new_proxies:
+                file.write(proxy + "\n")
+
+    # 检查文件行数，如果超过100行，则删除前50行
+    with open(filename, "r") as file:
+        lines = file.readlines()
+
+    if len(lines) > 400:
+        with open(filename, "w") as file:
+            file.writelines(lines[200:])  # 保留从第200行到最后的行
+
+
+def task_check_proxies():
+    # 下载文件
+    download_file()
+    # 获取待检查的proxy列表
+    proxies, proxy_url_infos = generate_proxies()
+    print(f"start checking {len(proxies)} proxies")
+    available_proxies = []
+    for proxy in proxies:
+        # print(f"checking {proxy}")
+        if check_proxy(proxy, proxy_url_infos):
+            available_proxies.append(proxy)
+            now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            print(f"{now} Available proxies: {len(available_proxies)}")
+            # 更新文件
+            update_proxy_file(FILENAME, available_proxies)
+            # 上传到GIT
+            upload_file_to_github(FILENAME)
+        else:
+            pass
+        if len(available_proxies) >= 5:
+            break
+        else:
+            continue
+    print("check end.")
+
+
+# 上传文件（相当于推送）
+def upload_file_to_github(filename):
+    token = os.environ['GIT_TOKEN']
+    repo = 'claude89757/free_https_proxies'
+    url = f'https://api.github.com/repos/{repo}/contents/{FILENAME}'
+
+    headers = {
+        'Authorization': f'token {token}',
+        'Accept': 'application/vnd.github.v3+json'
+    }
+    with open(FILENAME, 'rb') as file:
+        content = file.read()
+    data = {
+        'message': f'Update proxy list by scf',
+        'content': base64.b64encode(content).decode('utf-8'),
+        'sha': get_file_sha(url, headers)  # You need to get the SHA if you're updating an existing file
+    }
+    response = requests.put(url, headers=headers, json=data)
+    if response.status_code == 200:
+        print("File uploaded successfully.")
+    else:
+        print("Failed to upload file:", response.status_code, response.text)
+
+
+def download_file():
+    """
+    从指定的 URL 下载文件并保存到 /tmp 目录下，如果存在同名文件则覆盖。
+    """
+    # 完整的文件路径
+    try:
+        # 发送 GET 请求
+        url = 'https://raw.githubusercontent.com/claude89757/free_https_proxies/main/free_https_proxies.txt'
+        response = requests.get(url)
+        response.raise_for_status()  # 确保请求成功，否则抛出异常
+
+        # 写入文件
+        with open(FILENAME, 'wb') as file:
+            file.write(response.content)
+        print(f"File downloaded and saved to {FILENAME}")
+    except requests.RequestException as e:
+        print(f"Failed to download the file: {e}")
+
+
+def get_file_sha(url, headers):
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()['sha']
+    return None
+
+
+if __name__ == '__main__':
+    task_check_proxies()
---
+++ proxy_tools/pass_slide_captcha.py
@@ -0,0 +1,258 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2024-09-13
+@Author  : claude
+@File    : pass_slide_captcha.py
+@Software: PyCharm
+"""
+
+import os
+import time
+import random
+import requests
+import json
+import base64
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+FILENAME = "isz_https_proxies_infos.json"
+
+
+def generate_proxies():
+    """
+    获取待检查的代理列表
+    """
+    urls = [
+        "https://raw.githubusercontent.com/claude89757/free_https_proxies/main/isz_https_proxies.txt"
+    ]
+    proxies = []
+
+    for url in urls:
+        print(f"getting proxy list for {url}")
+        response = requests.get(url)
+        # 将文本内容按行分割，并去除每行两端的空格
+        text = response.text.strip()
+        lines = text.split("\n")
+        lines = [line.strip() for line in lines]
+        proxies.extend(lines)
+        print(f"Loaded {len(lines)} proxies from {url}")
+    print(f"Total {len(proxies)} proxies loaded")
+    random.shuffle(proxies)
+    return proxies
+
+
+# 上传文件（相当于推送）
+def upload_file_to_github(data_list: list):
+    token = os.environ['GIT_TOKEN']
+    repo = 'claude89757/free_https_proxies'
+    url = f'https://api.github.com/repos/{repo}/contents/{FILENAME}'
+
+    headers = {
+        'Authorization': f'token {token}',
+        'Accept': 'application/vnd.github.v3+json'
+    }
+
+    # 把data_list转换成content
+    content = json.dumps(data_list)
+    encoded_content = base64.b64encode(content.encode('utf-8')).decode('utf-8')
+
+    data = {
+        'message': f'Update proxy list by scf',
+        'content': encoded_content,
+        'sha': get_file_sha(url, headers)  # You need to get the SHA if you're updating an existing file
+    }
+    response = requests.put(url, headers=headers, json=data)
+    if response.status_code == 200:
+        print(F"File uploaded successfully, total {len(data_list)} proxies")
+    else:
+        print("Failed to upload file:", response.status_code, response.text)
+
+
+def get_file_sha(url, headers):
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()['sha']
+    return None
+
+
+class TwitterWatcher:
+    def __init__(self, timeout=10, headless: bool = True):
+        self.driver_path = "/usr/local/bin/chromedriver"
+        self.timeout = timeout
+        self.interaction_timeout = 10
+        self.driver = None
+        self.headless = headless
+
+    def setup_driver(self, proxy=None):
+        service = Service(self.driver_path)
+        chrome_options = Options()
+        chrome_options.add_argument("--lang=cn")
+        if self.headless:
+            chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--disable-gpu")
+            chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+
+        # 定义多个 User-Agent 字符串
+        user_agents = [
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/128.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/91.0.4472.124 Safari/537.36",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/128.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+            "Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) "
+            "Version/14.1.1 Mobile/15E148 Safari/604.1",
+            "Mozilla/5.0 (iPad; CPU OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) "
+            "Version/14.1.1 Mobile/15E148 Safari/604.1",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) "
+            "Version/14.1.1 Safari/605.1.15",
+            "Mozilla/5.0 (X11; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0"
+        ]
+
+        # 随机选择一个 User-Agent
+        random_user_agent = random.choice(user_agents)
+        chrome_options.add_argument(f"user-agent={random_user_agent}")
+
+        if proxy:
+            chrome_options.add_argument(f'--proxy-server={proxy}')
+
+        self.driver = webdriver.Chrome(service=service, options=chrome_options)
+
+    def teardown_driver(self):
+        if self.driver:
+            self.driver.quit()
+
+    def random_delay(self, min_delay=1, max_delay=3):
+        time.sleep(random.uniform(min_delay, max_delay))
+
+    def wait_for_element(self, by, value, timeout=None):
+        timeout = timeout or self.timeout
+        return WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located((by, value)))
+
+    def wait_for_url_change(self, current_url, timeout=None):
+        timeout = timeout or self.timeout
+        WebDriverWait(self.driver, timeout).until(EC.url_changes(current_url))
+
+    def solve_slider_captcha(self):
+        slider = self.wait_for_element(By.CLASS_NAME, "btn_slide")
+        action_chains = ActionChains(self.driver)
+
+        # 点击并按住滑块
+        action_chains.click_and_hold(slider).perform()
+        time.sleep(random.uniform(0.5, 1.0))  # 模拟人类按住滑块的时间
+
+        # 模拟人类滑动的过程
+        total_offset = 300  # 滑动的总距离
+        current_offset = 0
+        while current_offset < total_offset:
+            move_by = random.randint(10, 30)  # 每次滑动的距离
+            action_chains.move_by_offset(move_by, 0).perform()
+            current_offset += move_by
+            time.sleep(random.uniform(0.01, 0.03))  # 每次滑动后的短暂延迟
+
+        # 释放滑块
+        action_chains.release().perform()
+        time.sleep(random.uniform(0.5, 1.0))  # 模拟人类释放后的时间
+
+    def transfer_cookies(self):
+        cookies = self.driver.get_cookies()
+        session = requests.Session()
+        for cookie in cookies:
+            session.cookies.set(cookie['name'], cookie['value'])
+        return session
+
+    def transfer_headers(self):
+        headers = {
+            'User-Agent': self.driver.execute_script("return navigator.userAgent;")
+        }
+        return headers
+
+
+if __name__ == '__main__':
+    """
+    缓存各代理访问ISZ的方式
+    """
+    output_data = []
+    for server_and_port in generate_proxies():
+        watcher = TwitterWatcher(headless=True)
+        proxy = f"http://{server_and_port}"
+        watcher.setup_driver(proxy=proxy)
+        try:
+            url = os.environ['ISZ_URL']
+            watcher.driver.get(url)
+
+            # 随机延迟模拟人类行为
+            watcher.random_delay()
+
+            # 等待页面加载完成
+            watcher.wait_for_element(By.TAG_NAME, "body")
+
+            # 打印当前网页的内容
+            print(watcher.driver.page_source)
+
+            if "签名错误,接口未签名" in str(watcher.driver.page_source):
+                # 无需滑块验证
+                current_url = watcher.driver.current_url
+                print(f"Current URL: {current_url}")
+                output_data.append({
+                    "proxy": server_and_port,
+                    "type": "direct",
+                    "target_url": current_url,
+                })
+            else:
+                # 解决滑块验证
+                watcher.solve_slider_captcha()
+
+                # 等待页面加载完成
+                WebDriverWait(watcher.driver, 30).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
+
+                # 打印当前网页的内容
+                print(watcher.driver.page_source)
+
+                # 获取并打印当前的URL
+                current_url = watcher.driver.current_url
+                print(f"Current URL: {current_url}")
+
+                # 使用requests通过代理请求current_url
+                cookies = watcher.driver.get_cookies()
+                session = requests.Session()
+                for cookie in cookies:
+                    session.cookies.set(cookie['name'], cookie['value'])
+                headers = {
+                    'User-Agent': watcher.driver.execute_script("return navigator.userAgent;")
+                }
+                print(f"cookies: {cookies}")
+                print(f"headers: {headers}")
+                proxies = {
+                    "http": proxy,
+                    "https": proxy,
+                }
+                response = session.get(current_url, headers=headers, proxies=proxies)
+                print(f"Response from {current_url}:")
+                print(response.text)
+
+                output_data.append({
+                    "proxy": server_and_port,
+                    "type": "by cookies",
+                    "target_url": current_url,
+                    "cookies": cookies,
+                    "headers": headers,
+                })
+
+        except Exception as error:
+            print(f"proxy {server_and_port} failed: {error}")
+        finally:
+            watcher.teardown_driver()
+    upload_file_to_github(output_data)
