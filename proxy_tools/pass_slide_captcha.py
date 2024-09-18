8166a9b5ba0e691533a7888204cced1fdd09ce3f
claude89757 <claudexie@outlook.com>
2024-09-19 00:12:02 +0800

    fix

--- proxy_tools/pass_slide_captcha.py
+++ proxy_tools/pass_slide_captcha.py
@@ -209,6 +209,9 @@ if __name__ == '__main__':
             # 等待页面加载完成
             watcher.wait_for_element(By.TAG_NAME, "body")
 
+            print(f"Current URL: {watcher.driver.current_url}")
+            print(f"page: {str(watcher.driver.page_source)}")
+            
             if "签名错误" in str(watcher.driver.page_source):
                 print(f"[1] processing directly...")
                 # 无需滑块验证
