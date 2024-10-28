1b4ccc281e65514580ae8e1a33e6ba9a343b3d1b
claude89757 <138977524+claude89757@users.noreply.github.com>
2024-10-28 12:18:35 +0800

    fix

--- customization/sh_001_watcher/sh_001_watcher.py
+++ customization/sh_001_watcher/sh_001_watcher.py
@@ -339,8 +339,9 @@ if __name__ == '__main__':
             court_name = up_for_send_data['court_name']
             free_slot_list = up_for_send_data['free_slot_list']
             for free_slot in free_slot_list:
-                cache_key = f"{court_name}_{date}_{free_slot[0]}"
-                if cache_key in cache:
+                cache_key_001 = f"{court_name}_{date}_{free_slot[0]}"
+                cache_key_002 = f"{court_name}_{date}_{free_slot[1]}"
+                if cache_key_001 in cache or cache_key_002 in cache:
                     # 已通知过
                     pass
                 else:
@@ -352,7 +353,8 @@ if __name__ == '__main__':
                                                  "end_time": free_slot[1],
                                                  })
                     # 更新本地文件缓存
-                    cache[cache_key] = 1
+                    cache[cache_key_001] = 1
+                    cache[cache_key_002] = 1
         # 关闭本地文件缓存
         cache.close()
 
